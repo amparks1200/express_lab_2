757a631282210f278a8839294b4ff95715e9b87c
Alessia Hoey <alessiamich747@gmail.com>
2021-03-01 09:51:35 -0500

    second commit for express lab 2

--- cart-items.js
+++ cart-items.js
@@ -19,6 +19,9 @@ routes.get("/cart-items", function(req, res) {
         });
 // responds with JSON array of indicated page size
     } if (pageSize) {
+        pool.query('SELECT * FROM shopping_cart WHERE count<=$1', [pageSize].then(results => {
+            res.json(results.rows);
+        }))
 // responds with JSON array of all cart items & status code 200
     } else {
     pool.query("SELECT * FROM shopping_cart order by id").then(result =>
@@ -29,17 +32,38 @@ routes.get("/cart-items", function(req, res) {
 }});
 
 routes.get("/cart-items/:id", function(req, res) {
-    
-});
+    let item = cartItems.find(itemID => itemID.id === parseInt(req.params.id));
+// responds with JSON object of the item with the given ID
+    if (item) {
+        pool.query('SELECT * FROM shopping_cart WHERE id<=$1', [item].then(results => {
+        res.json(results.rows);
+// responds with status code 200
+        res.status(200);
+    })) 
+// responds with status code 404 when not found
+} else {
+    res.status(404);
+}});
+
 routes.post("/cart-items", function(req, res) {
 
 });
 
-routes.put("cart-items", function(req, res) {
+routes.put("cart-items/:id", function(req, res) {
 
 });
 
-routes.delete("cart-items", function(req, res) {
-
+routes.delete("cart-items/:id", function(req, res) {
+    let item = cartItems.find(itemID => itemID.id === parseInt(req.params.id));
+//Removes the itme from the database that has the given ID
+    if (item) {
+        pool.query('DELETE * FROM shopping_cart WHERE id<-$1', [item].then(results => {
+            res.json(results.rows);
+        }))
+// responds with no content and status code 204
+    } else {
+        res.status(204);
+    }
 });
+
 module.exports = routes;
